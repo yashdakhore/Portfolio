904e08713f9de6c06d87358a5e87945d17adb981
yashdakhore <yash.p.dakhore@gmail.com>
2025-07-07 03:32:07 +0530

    first commit

--- script.js
+++ script.js
@@ -20,37 +20,3 @@ gsap.from(".main-text h1", {
     stagger:.5,
     opacity:0,
 })
-
-
-
-
-// Theme Toggle (Dark/Light Mode)
-const themeToggle = document.getElementById("theme-toggle");
-const themeIcon = document.getElementById("theme-icon");
-
-// Load last theme from localStorage (optional)
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  themeIcon.classList.remove("ri-moon-line");
-  themeIcon.classList.add("ri-sun-line");
-  themeIcon.style.color = "white";
-} else {
-  themeIcon.classList.remove("ri-sun-line");
-  themeIcon.classList.add("ri-moon-line");
-  themeIcon.style.color = "black";
-}
-
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  if (document.body.classList.contains("dark-mode")) {
-    themeIcon.classList.remove("ri-moon-line");
-    themeIcon.classList.add("ri-sun-line");
-    themeIcon.style.color = "white";
-    localStorage.setItem("theme", "dark");
-  } else {
-    themeIcon.classList.remove("ri-sun-line");
-    themeIcon.classList.add("ri-moon-line");
-    themeIcon.style.color = "black";
-    localStorage.setItem("theme", "light");
-  }
-});
