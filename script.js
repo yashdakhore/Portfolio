3f8aef3c32c7dd7945e3daf78ff5bd9e7cd93b93
yashdakhore <yash.p.dakhore@gmail.com>
2025-07-07 21:27:25 +0530

    Website color schema completed

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ gsap.from(".main-text h1", {
     opacity:0,
     clearProps: "all"
 })
+
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    themeIcon.classList.remove('ri-sun-line');
+    themeIcon.classList.add('ri-moon-line');
+  } else {
+    themeIcon.classList.remove('ri-moon-line');
+    themeIcon.classList.add('ri-sun-line');
+  }
+});
+
+
+
+
+// Copy Email Button Functionality
+const copyBtn = document.getElementById('copy-btn');
+const emailText = document.getElementById('email-text');
+
+copyBtn.addEventListener('click', function() {
+  // Copy the email text to clipboard
+  navigator.clipboard.writeText(emailText.textContent.trim());
+
+  // Change button text to 'Copied!'
+  copyBtn.textContent = 'Copied!';
+
+  // Revert back to 'Copy' after 1.5 seconds
+  setTimeout(() => {
+    copyBtn.textContent = 'Copy';
+  }, 1500);
+});
